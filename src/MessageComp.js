5e8ce80fbcabafcedce50924891f28401b2a07bc
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-09-16 17:41:33 +0300

    upd

--- src/MessageComp.js
+++ src/MessageComp.js
@@ -8,6 +8,7 @@ export default function MessageComp(props) {
 	return (
 		<div className='message-class'>
 			<p>{props.name}, спасибо за знания, которые Вы нам даете!</p>
+			<p className='text-class'>React Devtools установлен!!!!</p>
 		</div>
 	)
 }
