cfe5becacc35e1c6e0407c3a7d1fedd3e447bc62
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-09-19 17:12:25 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import MessageComp from "./MessageComp";
 
+const messData = {
+	name: "Андрей",
+	messText: ', спасибо за знания, которые Вы нам даете!',
+	devToolsInstText: 'React Devtools установлен!!!!',
+	img: "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google_Chrome-256.png"
+}
+
 function App() {
 	return (
-		<div className="App">
-			<MessageComp name="Андрей" />
+		<div className="app">
+			<MessageComp data={messData} />
 		</div>
 	);
 }
 
-export default App;
+export default App;  
--- src/MessageComp.js
+++ src/MessageComp.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './MessageComp.css';
 
 
-const messText = ', спасибо за знания, которые Вы нам даете!';
-const devToolsInst = 'React Devtools установлен!!!!';
+
 
 export default function MessageComp(props) {
 
 	return (
 		<div className='message-class'>
-			<p>{props.name} {messText}</p>
-			<p className='text-class'>{devToolsInst}</p>
+			<p>{props.data.name} {props.data.messText}</p>
+			<img src={props.data.img} alt="logo" />
+			<p className='text-class'>{props.data.devToolsInstText}</p>
 		</div>
 	)
 }
