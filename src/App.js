68cba769c394b23c73861d5b938d3a1836d99dbb
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-09-19 17:54:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import MessageComp from "./MessageComp";
 
-const messData = {
-	name: "Андрей",
-	messText: ', спасибо за знания, которые Вы нам даете!',
-	devToolsInstText: 'React Devtools установлен!!!!',
-	img: "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google_Chrome-256.png"
-}
+
 
 function App() {
+
+	const messData = {
+		name: "Андрей",
+		messText: ', спасибо за знания, которые Вы нам даете!',
+		devToolsInstText: 'React Devtools установлен!!!!',
+		img: "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google_Chrome-256.png"
+	}
+
 	return (
 		<div className="app">
 			<MessageComp data={messData} />
